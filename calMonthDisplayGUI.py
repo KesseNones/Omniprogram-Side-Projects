eb1f42c920b2e8fa5c9290f24c6a0267b3a7bf37
KesseNones <jesse.jones@wsu.edu>
2023-05-09 14:08:47 -0700

    Refactored and commented calender month displayer
    Comments were added to improve readability. The font size was also
    reduced to improve friendlyness with smaller displays.
    Some code was also refactored to be less cringe. The calculation
    of the week day is also done by an imported module instead of a
    copy pasted fuction now.

--- calMonthDisplayGUI.py
+++ calMonthDisplayGUI.py
@@ -1,11 +1,18 @@
+#Jesse A. Jones
+#Version: 2023-05-09.81
+
 from tkinter import *
 import math
 import time
 import datetime
-from tkinter import messagebox
-import calendar
+import leapDetect
+import dateHandling
+import weekCalculator
 
+#This class displays a calendar month page based 
+#   on an input year and input month.
 class CalendarDisp(object):
+    #Sets up GUI.
     def __init__(self, window = None):
         self.window = window
 
@@ -19,33 +26,39 @@ class CalendarDisp(object):
         self.frameBottom = Frame(self.window)
         self.frameBottom.pack(side = BOTTOM)
 
-        self.messageI = Label(self.frameBottom, text = "Enter Year:", font = "Times 55")
+        self.messageI = Label(self.frameBottom, text = "Enter Year:", font = "Ariel 20")
         self.messageI.grid(row = 0, column = 0)
 
-        self.yearE = Entry(self.frameBottom, font = "Times 45")
-        self.yearE.grid(row = 0, column = 1)
+        self.year = Entry(self.frameBottom, font = "Ariel 20")
+        self.year.grid(row = 0, column = 1)
 
-        self.messageII = Label(self.frameBottom, text = "Enter Month:", font = "Times 55")
+        self.messageII = Label(self.frameBottom, text = "Enter Month:", font = "Ariel 20")
         self.messageII.grid(row = 2, column = 0)
 
-        self.monthE = Entry(self.frameBottom, font = "Times 45")
-        self.monthE.grid(row = 2, column = 1)
+        self.month = Entry(self.frameBottom, font = "Ariel 20")
+        self.month.grid(row = 2, column = 1)
     
         self.convButton = Button(self.frameBottom, text = "Show Month", 
-            font = "Times 50", command = self.monthDisp)
+            font = "Ariel 20", command = self.monthDisp)
         self.convButton.grid(row = 3, column = 0)
 
         self.cOutput = Label(self.frameBottom, text = "", 
-            font = "Times 40", justify = LEFT, anchor = "w")
+            font = "Ariel 20", justify = LEFT, anchor = "w")
         self.cOutput.grid(row = 3, column = 1)
 
+        self.dateParse = dateHandling.GetDate()
+        self.leaper = leapDetect.IsLeap()
+        self.weekCalc = weekCalculator.WeekFinder()
+
+    #Quits program.
     def quitButtonAction(self):
         self.window.destroy()
 
-    def cal_disp(self, year, month):
+    #Creates month display string based on input year and month.
+    def calDisp(self, year, month):
         year = int(year)
         month = int(month)
-        if month == 1:
+        if month == 1:                                                                      
             m = "January"
         if month == 2:
             m = "February"
@@ -69,7 +82,7 @@ class CalendarDisp(object):
             m = "November"
         if month == 12:
             m = "December"
-        startingDay = self.week_fdn(year, month, 1)
+        startingDay = self.weekCalc.weekFind(year, month, 1)
         calDeeta = self.calCalc(year, month, startingDay)
         monthDisp = "   " + m + " " + str(year) + "\n Mo Tu We Th Fr Sa Su \n"
         for row in range(6):
@@ -78,6 +91,7 @@ class CalendarDisp(object):
             monthDisp += "\n"
         return monthDisp
 
+    #Determines if input year is a leap year or not.
     def isLeapYear(self, year):
         if year % 4 == 0:
             leap = True
@@ -89,7 +103,9 @@ class CalendarDisp(object):
             leap = False
         return leap
 
+    #Creates the matrix that represents the calendar month page.
     def calCalc(self, year, month, weekDayStart):
+        #Determines month length.
         leap = self.isLeapYear(year)
         if month == 1 or month == 3 or month == 5 or month == 7 or month == 8 or month == 10 or month == 12:
             dayMax = 31
@@ -99,9 +115,13 @@ class CalendarDisp(object):
             dayMax = 28
         if month == 2 and leap:
             dayMax = 29
+
+        #Month matrix initialized.
         monthMatrix = [["-", "-", "-", "-", "-", "-", "-"],["-", "-", "-", "-", "-", "-", "-"],
             ["-", "-", "-", "-", "-", "-", "-"],["-", "-", "-", "-", "-", "-", "-"],
             ["-", "-", "-", "-", "-", "-", "-"],["-", "-", "-", "-", "-", "-", "-"]]
+        
+        #Determines what start of month looks like.
         if weekDayStart == "Monday":
             monthMatrix[0] = ["01", "02", "03", "04", "05", "06" , "07"]
         if weekDayStart == "Tuesday":
@@ -116,7 +136,11 @@ class CalendarDisp(object):
             monthMatrix[0] = ["    ", "    ", "    ", "    ", "    ", "01", "02"]
         if weekDayStart == "Sunday":
             monthMatrix[0] = ["    ", "    ", "    ", "    ", "    ", "    ", "01"]
+        
+        #Calculates date to start week 2 and beyond of month with.
         num = int(monthMatrix[0][6]) + 1
+
+        #Fills in the rest of the month matrix based on num.
         for row in range(1, 6):
             for day in range(7):
                 if num <= dayMax:
@@ -125,91 +149,16 @@ class CalendarDisp(object):
                 else:
                     monthMatrix[row][day] = "    "
         return monthMatrix
-        
-    def week_fdn(self, year, month, day):
-        subtract = 0
-        if (month < 3) and ((year % 4) == 0 or (year % 400) == 0):
-            subtract = -1
-            if year % 100 == 0:
-                subtract = 0
-            if (year % 100 == 0) and (year % 400 == 0):
-                subtract = -1
-        if (month > 2):
-            subtract = 0
-        y = year % 100
-        yII = int(y / 4)
-        yIII = yII + y
-        yC = yIII % 7
-        if (year - y) % 400 == 0:
-            cC = 6
-        if ((year - y) - 100) % 400 == 0:
-            cC = 4
-        if ((year - y) - 200) % 400 == 0:
-            cC = 2
-        if ((year - y) - 300) % 400 == 0:
-            cC = 0
-        net = yC + cC
-        if month == 1:
-            mC = 0
-        if month == 2:
-            mC = 3
-        if month == 3:
-            mC = 3
-        if month == 4:
-            mC = 6
-        if month == 5:
-            mC = 1
-        if month == 6:
-            mC = 4
-        if month == 7:
-            mC = 6
-        if month == 8:
-            mC = 2
-        if month == 9:
-            mC = 5
-        if month == 10:
-            mC = 0
-        if month == 11:
-            mC = 3
-        if month == 12:
-            mC = 5
-        net = net + mC
-        net = net + int(day)
-        net = net + subtract
-        net = net % 7
-        if net == 0:
-            wk = "Sunday"
-        if net == 1:
-            wk = "Monday"
-        if net == 2:
-            wk = "Tuesday"
-        if net == 3:
-            wk = "Wednesday"
-        if net == 4:
-            wk = "Thursday"
-        if net == 5:
-            wk = "Friday"
-        if net == 6:
-            wk = "Saturday"
-        return wk
 
+    #Called when display month button is pressed. 
+    #   Actually displays the calendar month.
     def monthDisp(self):
-        year = self.yearGet()
-        month  = self.monthGet()
-        cal = self.cal_disp(year, month)
-        self.cOutput["text"] = cal
-
-    def yearGet(self):
-        if self.yearE.get() == "":
-            messagebox.showerror("Empty Entry Error", "Be sure to enter a year!")
-        else:
-            return int(self.yearE.get())
-    
-    def monthGet(self):
-        if self.monthE.get() == "":
-            messagebox.showerror("Empty Entry Error", "Be sure to enter a month!")
-        else:
-            return int(self.monthE.get())
+        #Parses input year and month.
+        year = self.dateParse.getYear(self.year.get())
+        month  = self.dateParse.getMonth(self.month.get())
+        
+        #Calculates and displays month.
+        self.cOutput["text"] = self.calDisp(year, month)
 
 def main():
     root = Tk()
