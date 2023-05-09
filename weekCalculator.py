3dc506f9c97e6c62817609c69a8a17bcc047eaef
KesseNones <jesse.jones@wsu.edu>
2023-05-09 13:58:18 -0700

    Modifyied weekCalculator importable to be a class

--- weekCalculator.py
+++ weekCalculator.py
@@ -3,22 +3,25 @@
 
 import leapDetect
 
-leap = leapDetect.IsLeap()
+#This class calculates a weekday of a date passed in it.
+class WeekFinder():
+	def __init__():
+		self.leap = leapDetect.IsLeap()
 
-#Finds day of week from input year, month, and day.
-def weekFind(year, month, day):
-    #Determines if 1 needs to be subtracted from calculations or not.
+	#Finds day of week from input year, month, and day.
+	def weekFind(year, month, day):
+	    #Determines if 1 needs to be subtracted from calculations or not.
 
-    #Useful arrays for calculating week stuff.
-    centuryCodeArr = [6, 4, 2, 0]
-    monthCodeArr = [0, 3, 3, 6, 1, 4, 6, 2, 5, 0, 3, 5]
-    weekNameArr = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+	    #Useful arrays for calculating week stuff.
+	    centuryCodeArr = [6, 4, 2, 0]
+	    monthCodeArr = [0, 3, 3, 6, 1, 4, 6, 2, 5, 0, 3, 5]
+	    weekNameArr = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
 
-    #Numbers used in the week calculation.
-    leapSubtract = 0 - ((month < 3) and leap.isLeapYear(year))
-    cenCode = centuryCodeArr[(year // 100) % 4]
-    monthCode = monthCodeArr[month - 1]
-    decAndYr = year % 100
-    yearNum = decAndYr + (decAndYr // 4)
+	    #Numbers used in the week calculation.
+	    leapSubtract = 0 - ((month < 3) and self.leap.isLeapYear(year))
+	    cenCode = centuryCodeArr[(year // 100) % 4]
+	    monthCode = monthCodeArr[month - 1]
+	    decAndYr = year % 100
+	    yearNum = decAndYr + (decAndYr // 4)
 
-    return weekNameArr[((cenCode + yearNum + monthCode + day + leapSubtract) % 7)]
+	    return weekNameArr[((cenCode + yearNum + monthCode + day + leapSubtract) % 7)]
