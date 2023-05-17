8a374f3772532047fe229d57c2bf29e004414119
KesseNones <jesse.jones@wsu.edu>
2023-05-16 20:27:02 -0700

    Refactored Eight Day Portion Clock
    The clock still performs the same function but the code was
    optimized to use lists and indexing instead of a large conditional
    chain. Generally the code is cleaner and less cursed now.

--- eightPortionClock.py
+++ eightPortionClock.py
@@ -1,5 +1,5 @@
 #Jesse A. Jones
-#Version: 2023-05-13.20
+#Version: 2023-05-17.13
 
 from tkinter import *
 import datetime
@@ -24,11 +24,11 @@ class EightClock(object):
         self.frameBottom.pack(side = BOTTOM)
 
         #3 hour time
-        self.messageTime = Label(self.frameBottom, text = "", font = "Ariel 75", anchor = "w")
+        self.messageTime = Label(self.frameBottom, text = "", font = "Ariel 50", anchor = "w")
         self.messageTime.grid(row = 0, column = 0)
 
         #3 hour time label.
-        self.messageTimeSeg = Label(self.frameBottom, text = "", font = "Ariel 75", anchor = "w", bg = "#373737")
+        self.messageTimeSeg = Label(self.frameBottom, text = "", font = "Ariel 50", anchor = "w", bg = "#373737")
         self.messageTimeSeg.grid(row = 1, column = 0)
 
         #Starts recursive time display loop.
@@ -40,75 +40,39 @@ class EightClock(object):
 
     #Fetches current 3 hour time and displays it.
     def timeUpdate(self):
-        time = self.convFunTime()
-        self.messageTime["text"] = time
-        self.messageTimeSeg["text"] = self.portion
-        self.window.after(1, self.dateUpdate)
+        timeArr = self.convEightTime()
+        #Displays converted time.
+        self.messageTime["text"] = timeArr[0]
+        self.messageTimeSeg["text"] = timeArr[1]
+        self.messageTimeSeg["fg"] = timeArr[2]
 
-    #Gets the current time.
-    def currentTime(self):
-        self.local = datetime.datetime.now()
-        self.hourUnalt = self.local.hour
-        self.min = self.local.minute
-        self.sec = self.local.second
+        #Recursively loops time.
+        self.window.after(1, self.timeUpdate)
 
     #Given current time, conversion to 3 hour time happens.
-    def convFunTime(self):
+    def convEightTime(self):
         #Fetches current time.
-        self.currentTime()
+        local = datetime.datetime.now()
+        hour = local.hour
+        minute = local.minute
+        second = local.second
 
-        #Late night case.
-        if 0 <= self.hourUnalt < 3:
-            portion = "Late Night"
-            hour = self.hourUnalt + 1
-            self.messageTimeSeg["fg"] = "black"
+        #Sets up important lists for time display.
+        poritonArr = ["Late Night", "Pre Morning", "Morning", "Late Morning", 
+                    "Afternoon", "Late Afternoon", "Evening", "Night"]
+        colorArr = ["black", "#00857d", "#ffff3f", "#00ff9b",
+                    "#93ffd5", "#b6ff54", "#0e5efe", "#03006a"]
         
-        #Pre-morning case.
-        if 3 <= self.hourUnalt < 6:
-            portion = "Pre-Morning"
-            hour = (self.hourUnalt - 3) + 1
-            self.messageTimeSeg["fg"] = "#00857d"
+        #Calculates index for lists and new hour.
+        index = hour // 3
+        eightHour = (hour - (3 * index)) + 1
 
-        #Morning case
-        if 6 <= self.hourUnalt < 9:
-            portion = "Morning"
-            hour = (self.hourUnalt - 6) + 1
-            self.messageTimeSeg["fg"] = "#ffff3f"
+        #Time and portion info set.
+        timeString = str(eightHour) + ":" + str(minute).zfill(2) + ":" + str(second).zfill(2)
+        portionText = poritonArr[index]
+        portionColor = colorArr[index]
         
-        #Late morning case.
-        if 9 <= self.hourUnalt < 12:
-            portion = "Late Morning"
-            hour = (self.hourUnalt - 9) + 1
-            self.messageTimeSeg["fg"] = "#00ff9b"
-        
-        #Afternoon case.
-        if 12 <= self.hourUnalt < 15:
-            portion = "Afternoon"
-            hour = (self.hourUnalt - 12) + 1
-            self.messageTimeSeg["fg"] = "#93ffd5"
-        
-        #Late afternoon case.
-        if 15 <= self.hourUnalt < 18:
-            portion = "Late Afternoon"
-            hour = (self.hourUnalt - 15) + 1
-            self.messageTimeSeg["fg"] = "#b6ff54"
-    
-        #Evening case.
-        if 18 <= self.hourUnalt < 21:
-            portion = "Evening"
-            hour = (self.hourUnalt - 18) + 1
-            self.messageTimeSeg["fg"] = "#0e5efe"
-        
-        #Night case.
-        if 21 <= self.hourUnalt < 24:
-            portion = "Night"
-            hour = (self.hourUnalt - 21) + 1
-            self.messageTimeSeg["fg"] = "#03006a"
-            
-        #Sets appropriate portion and generates time string.
-        self.portion = portion
-        timeString = str(hour) + ":" + str(self.min).zfill(2) + ":" + str(self.sec).zfill(2)
-        return timeString
+        return [timeString, portionText, portionColor]
 
 def main():
     root = Tk()
