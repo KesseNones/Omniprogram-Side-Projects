23bd24b18e671ea633c6f27892f70270ab1ca7ea
KesseNones <jesse.jones@wsu.edu>
2023-05-16 20:43:14 -0700

    Refactored Eight Portion Clock Calculator Code
    Made the code look less garbage by optimizing it. Very similar to
    how I optimized the code for the eight day portion clock itself.

--- eightPortionClockCalc.py
+++ eightPortionClockCalc.py
@@ -1,5 +1,5 @@
 #Jesse A. Jones
-#Version: 2023-05-17.12
+#Version: 2023-05-17.14
 
 from tkinter import *
 import dateHandling
@@ -26,20 +26,20 @@ class EightClockCalc(object):
         #Hour input field.
         self.messageI = Label(self.frameBottom, text = "Enter Hour:", font = "Ariel 20")
         self.messageI.grid(row = 0, column = 0)
-        self.hourE = Entry(self.frameBottom, font = "Ariel 20")
-        self.hourE.grid(row = 0, column = 1)
+        self.hour = Entry(self.frameBottom, font = "Ariel 20")
+        self.hour.grid(row = 0, column = 1)
 
         #Minute input field.
         self.messageII = Label(self.frameBottom, text = "Enter Minute:", font = "Ariel 20")
         self.messageII.grid(row = 2, column = 0)
-        self.minE = Entry(self.frameBottom, font = "Ariel 20")
-        self.minE.grid(row = 2, column = 1)
+        self.min = Entry(self.frameBottom, font = "Ariel 20")
+        self.min.grid(row = 2, column = 1)
 
         #Second input field.
         self.messageIII = Label(self.frameBottom, text = "Enter Second:", font = "Ariel 20")
         self.messageIII.grid(row = 3, column = 0)
-        self.secE = Entry(self.frameBottom, font = "Ariel 20")
-        self.secE.grid(row = 3, column = 1)
+        self.sec = Entry(self.frameBottom, font = "Ariel 20")
+        self.sec.grid(row = 3, column = 1)
     
         #Button that calls function to convert input time to 3 hour time.
         self.convButton = Button(self.frameBottom, text = "Convert to Eight Portion Clock", 
@@ -54,62 +54,43 @@ class EightClockCalc(object):
         self.messageTimeSeg = Label(self.frameBottom, text = "", font = "Ariel 50", anchor = "w", bg = "#373737")
         self.messageTimeSeg.grid(row = 6, column = 0)
 
+        #Used for input date parsing.
+        self.dateFind = dateHandling.GetDate()
+    
     #Quits program.
     def quitButtonAction(self):
         self.window.destroy()
 
     #Converts input time to eight day portion time.
     def clockConv(self):
-        time = self.convFunTime()
-        self.messageTime["text"] = time
-        self.messageTimeSeg["text"] = self.portion
+        timeArr = self.convEightTime()
+        self.messageTime["text"] = timeArr[0]
+        self.messageTimeSeg["text"] = timeArr[1]
+        self.messageTimeSeg["fg"] = timeArr[2]
 
-    #Fetches time input fields.
-    def timeGet(self):
-        dateFind = dateHandling.GetDate()
-        self.hourUnalt = dateFind.getHour(self.hourE.get())
-        self.minute = dateFind.getMinOrSec(self.minE.get())
-        self.second = dateFind.getMinOrSec(self.secE.get())
+    #Given current time, conversion to 3 hour time happens.
+    def convEightTime(self):
+        #Fetches input dates.
+        hour = self.dateFind.getHour(self.hour.get())
+        minute = self.dateFind.getMinOrSec(self.min.get())
+        second = self.dateFind.getMinOrSec(self.sec.get())
 
-    #Converts input time to eight day portion time 
-    #   and sets the display accordingly.
-    def convFunTime(self):
-        self.timeGet()
-        if 0 <= self.hourUnalt < 3:
-            portion = "Late Night"
-            hour = self.hourUnalt + 1
-            self.messageTimeSeg["fg"] = "black"
-        if 3 <= self.hourUnalt < 6:
-            portion = "Pre-Morning"
-            hour = (self.hourUnalt - 3) + 1
-            self.messageTimeSeg["fg"] = "#00857d"
-        if 6 <= self.hourUnalt < 9:
-            portion = "Morning"
-            hour = (self.hourUnalt - 6) + 1
-            self.messageTimeSeg["fg"] = "#ffff3f"
-        if 9 <= self.hourUnalt < 12:
-            portion = "Late Morning"
-            hour = (self.hourUnalt - 9) + 1
-            self.messageTimeSeg["fg"] = "#00ff9b"
-        if 12 <= self.hourUnalt < 15:
-            portion = "Afternoon"
-            hour = (self.hourUnalt - 12) + 1
-            self.messageTimeSeg["fg"] = "#93ffd5"
-        if 15 <= self.hourUnalt < 18:
-            portion = "Late Afternoon"
-            hour = (self.hourUnalt - 15) + 1
-            self.messageTimeSeg["fg"] = "#b6ff54"
-        if 18 <= self.hourUnalt < 21:
-            portion = "Evening"
-            hour = (self.hourUnalt - 18) + 1
-            self.messageTimeSeg["fg"] = "#0e5efe"
-        if 21 <= self.hourUnalt < 24:
-            portion = "Night"
-            hour = (self.hourUnalt - 21) + 1
-            self.messageTimeSeg["fg"] = "#03006a"
-        self.portion = portion
-        timeString = str(hour) + ":" + str(self.minute).zfill(2) + ":" + str(self.second).zfill(2)
-        return timeString
+        #Sets up important lists for time display.
+        poritonArr = ["Late Night", "Pre Morning", "Morning", "Late Morning", 
+                    "Afternoon", "Late Afternoon", "Evening", "Night"]
+        colorArr = ["black", "#00857d", "#ffff3f", "#00ff9b",
+                    "#93ffd5", "#b6ff54", "#0e5efe", "#03006a"]
+        
+        #Calculates index for lists and new hour.
+        index = hour // 3
+        eightHour = (hour - (3 * index)) + 1
+
+        #Time and portion info set.
+        timeString = str(eightHour) + ":" + str(minute).zfill(2) + ":" + str(second).zfill(2)
+        portionText = poritonArr[index]
+        portionColor = colorArr[index]
+        
+        return [timeString, portionText, portionColor]
 
 def main():
     root = Tk()
