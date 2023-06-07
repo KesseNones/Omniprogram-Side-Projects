0a9c218a3d80549c5fb3f34be226b289097a942d
KesseNones <jesse.jones@wsu.edu>
2023-06-06 19:14:04 -0700

    Added comments to reformed Calendar page

--- reformedCalendarLive.py
+++ reformedCalendarLive.py
@@ -1,50 +1,75 @@
+#Jesse A. Jones
+#Version: 2023-06-07.07
+
 from tkinter import *
 import datetime
 import time
 import leapDetect
 import metricTime
 
+#This class displays the current date 
+#	in the reformed calendar in the form of a calendar page.
 class RefCalLive(object):
 	def __init__(self, window = None):
 		self.window = window
 
+		#Holds quit button.
 		self.frameTop = Frame(self.window)
 		self.frameTop.pack(side = TOP)
 
+		#Quits program when pressed.
 		self.quitButton = Button(self.frameTop, text = "Quit",
 			font = "Ariel 20", command = self.quitButtonAction)
 		self.quitButton.grid(row = 0, column = 0)
 
+		#Holds calendar page display.
 		self.frameBottom = Frame(self.window)
 		self.frameBottom.pack(side = BOTTOM)
 
+		#Used in leap year detection and day of year calculation.
 		self.leap = leapDetect.IsLeap()
 		self.metricAssist = metricTime.MetricTime()
+
+		#Lists and boolean used in creation of reformed calendar page.
 		self.titleAndWeekArr = []
 		self.tileArray = []
 		self.isFirstCreated = False
 
+		#Starts recursive time update loop.
 		self.timeTrack()
 
+	#Quits program when called.
 	def quitButtonAction(self):
 		self.window.destroy()
 
+	#Gets the current date, converts to reformed calendar, and displays result.
 	def timeTrack(self):
+		#Fetches current date and time and stores them in lists.
 		self.localTime = datetime.datetime.now()
 		dateArr = [self.localTime.year, self.localTime.month, self.localTime.day]
 		timeArr = [self.localTime.hour, self.localTime.minute, self.localTime.second]
+		
+		#Creates calendar page if it hasn't been made yet.
 		if not(self.isFirstCreated):
 			self.createCalendarPage(dateArr)
 			self.isFirstCreated = True
 
+		#If it's 0:00 hours, 
+		#	destroy and remake the calendar page 
+		#	to update the day.
 		if (timeArr[0] == 0 and timeArr[1] == 0):
 			self.destroyCalendarPage()
 			self.createCalendarPage(dateArr)
 
+		#Recursively loops after a second.
 		self.window.after(1000, self.timeTrack)
 
+	#Creates calendar page based on input date array.
 	def createCalendarPage(self, dateArray):
+		#Calculates reformed calendar date based on input date.
 		calArr = self.calcRef(dateArray)
+		
+		#Finds month index and establishes quad list and week day name list.
 		monthIndex = calArr[1] - 1 
 		rMonthNameArr = ["Unuary", "Duotober", "Tres", "Quadtober", "Quintecember", 
 			"Sixril", "Septecember", "Octo", "Novembuary", 
@@ -52,18 +77,25 @@ class RefCalLive(object):
 		rWeekNameArr = ["Solday", "Hermesday", "Venusday", 
 			"Terraday", "Lunaday", "Marsday", "Joviday"]
 
+		#Creates tile with information 
+		#	that represents the title of the calendar.
 		title = Tile(self.frameTop, 
 			f"{rMonthNameArr[monthIndex]} {calArr[0]} H.E.", 
 			1, 0, font = "Times 60", height = 2, width = 20)
 		self.titleAndWeekArr.append(title)
 
+		#If the index is the last one, 
+		#	a special supplementary calendar page is created. 
+		#	Otherwise, the standard 28 day page is made.
 		if (monthIndex == 13):
 			self.createSuppPage(calArr)
 		else:
+			#Creates weekday tiles.
 			for i in range(7):
 				weekNam = Tile(self.frameBottom, f"{rWeekNameArr[i]}", 1, i)
 				self.titleAndWeekArr.append(weekNam)
 
+			#Creates the 28 days of the month.
 			monthNum = 1
 			for x in range(4):
 				for y in range(7):
@@ -71,6 +103,7 @@ class RefCalLive(object):
 					self.tileArray.append(dayTile)
 					monthNum += 1
 
+		#Sets the current day to yellow and completed days to darker gray.
 		day = calArr[2]
 		pos = 1
 		while (pos < day):
@@ -78,8 +111,12 @@ class RefCalLive(object):
 			pos += 1
 		(self.tileArray[day - 1]).msgTile["bg"] = "yellow"
 
+	#Creates the supplementary calendar page if needed.
 	def createSuppPage(self, calArr):
+		#The two week day names possible for supplementary.
 		specialWeekNamArr = ["Yearday", "Leapday"]
+
+		#Creates weekdays and the up to two days of the calendar.
 		for i in range(1 + self.leap.isLeapYear(calArr[0])):
 			weekNamSpecil = Tile(self.frameBottom, f"{specialWeekNamArr[i]}", 1, i)
 			self.titleAndWeekArr.append(weekNamSpecil)
@@ -87,6 +124,7 @@ class RefCalLive(object):
 			self.tileArray.append(dayTile)
 		return
 
+	#Calculates the reformed calendar based on input date.
 	def calcRef(self, dateArray):
 		year = dateArray[0]
 		month = dateArray[1]
@@ -99,6 +137,7 @@ class RefCalLive(object):
 
 		return [rYear, rMonth, rDay]
 
+	#Destroys the calendar page when called.
 	def destroyCalendarPage(self):
 		for el in self.titleAndWeekArr:
 			el.msgTile.destroy()
@@ -107,10 +146,15 @@ class RefCalLive(object):
 		self.titleAndWeekArr = []
 		self.tileArray = []
 
+#This class represents a tile of the calendar used 
+#	to hold a day number or piece of calendar information.
 class Tile():
 	def __init__(self, frame, text, row, col, 
 		background = "#f0f0f0", font = "Times 35", 
 		borderThicc = 1, rel = "solid", height = 2, width = 9):
+
+		#Label created and gridded to actually hold 
+		#	the information passed to the constructor.
 		self.msgTile = Label(frame, text = text, font = font, 
 			bg = background, borderwidth = borderThicc, 
 			relief = rel, height = height, width = width)
