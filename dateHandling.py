ed29d703f2550f7b75a4af439c186827bb3f3cd1
KesseNones <jesse.jones@wsu.edu>
2023-07-31 22:51:13 -0700

    Refactored DateHandling Greatly
    Using Try Except feature of python it became possible to have
    much better input parsing that would put in the default values
    if a cast failed. Now it's much more versitile.

--- dateHandling.py
+++ dateHandling.py
@@ -1,80 +1,66 @@
 #Jesse A. Jones
-#Version: 2022-10-19
+#Version: 2023-08-01.23
 
 #This class is used to handle date input.
 #GetDate is intended to be used as an imported class in other programs.
 class GetDate():
-
     #Used in parsing an input intendet to be a year.
     def getYear(self, year, isFloat = False):
-
-        #Handles empty string cases.
-        if (isFloat == False):
-            if year == "":
-                return 0
-            else:
-                return int(year)
+        if isFloat:
+            #Tries to cast to float. If it succeeds, casted number is returned. 
+            #   In event of failure, 0.0 is returned.
+            try:
+                return float(year) 
+            except ValueError:
+                return 0.0
         else:
-            if year == "":
-                return float(0)
-            else:
-                return float(year)
+            #Tries to cast to int. If it succeeds, casted number is returned. 
+            #   In event of failure, 0 is returned.
+            try:
+                return int(year)
+            except ValueError:
+                return 0
     
     #Used in parsing month input.
     def getMonth(self, month):
-        #Empty string case.
-        if month == "":
+        #If casting succeeds, further checking occurs, 
+        #   otherwise default value returned.
+        try:
+            intMonth = int(month)
+            return (intMonth if intMonth > 0 and intMonth < 13 else 1)
+        except ValueError:
             return 1
-        month = int(month)
 
-        #Month out of range case.
-        if month > 12 or month < 1:
-            return 1
-        else:
-            return month
-
-    #Used in parsing day input.
+    #Parses input day string and returns casted valid day 
+    #   or default value of 1.
     def getDay(self, day):
-        #Day empty string case.
-        if day == "":
+        try:
+            intDay = int(day)
+            #If casting succeeds, range check occurs and returns 
+            #   either casted day or default value if out of range.
+            return (intDay if intDay > 0 and intDay < 32 else 1)
+        except ValueError:
             return 1
-        day = int(day)
 
-        #Day out of range case.
-        if day > 31 or day < 1:
-            return 1
-        else:
-            return day
-
-    #Used in parsing hour input.
+    #Parses input hour string given to it.
     def getHour(self, hour):
-        #Empty hour string case.
-        if hour == "":
+        try:
+            hourCast = int(hour)
+            return (hourCast if hourCast > -1 and hourCast < 24 else 0)
+        except ValueError:
             return 0
-        hour = int(hour)
-
-        #Out of range hour case.
-        if hour > 23 or hour < 0:
-            return 0
-        else:
-            return hour
 
     #Used in parsing minute or second input.
     def getMinOrSec(self, time):
-        #Empty minute/second case.
-        if time == "":
+        try:
+            timeCast = int(time)
+            return (timeCast if timeCast > -1 and timeCast < 60 else 0)
+        except ValueError:
             return 0
-        time = int(time)
-
-        #Out of range minute/second case.
-        if time > 60 or time < 0:
-            return 0
-        else:
-            return time
 
     #Used in processing general input.
     def getGeneral(self, inp):
-        if inp == "":
-            return 0
-        else:
+        try:
             return float(inp)
+        except ValueError:
+            return 0.0
