07d42cf790b7466ac32723f02f543d7f2049f8b7
KesseNones <jesse.jones@wsu.edu>
2023-08-14 14:59:08 -0700

    Made Font Smaller for mPrograms.py

--- mPrograms.py
+++ mPrograms.py
@@ -1,5 +1,5 @@
 #Jesse A. Jones
-#Version: 2023-06-13.17
+#Version: 2023-08-14.91
 
 from tkinter import *
 import metricDateGUILIVE
@@ -21,85 +21,87 @@ class M(object):
         self.frameTop = Frame(self.window)
         self.frameTop.pack(side = TOP)
 
+        FONT = "Ariel 20"
+
         self.quitButton = Button(self.frameTop, text = "Quit",
-            font = "Ariel 30", command = self.quitButtonAction)
+            font = FONT, command = self.quitButtonAction)
         self.quitButton.pack()
 
         self.frameBottom = Frame(self.window)
         self.frameBottom.pack(side = BOTTOM)
 
         self.metricButtonII = Button(self.frameBottom, text = "Metric Date",
-            font = "Ariel 30", command = self.metricDateLiveAct)
+            font = FONT, command = self.metricDateLiveAct)
         self.metricButtonII.grid(row = 0, column = 0)
 
         self.metricButton = Button(self.frameBottom, text = "Metric Date Calculator", 
-            font = "Ariel 30", command = self.metricCalculator)
+            font = FONT, command = self.metricCalculator)
         self.metricButton.grid(row = 0, column = 1)
 
         self.stopwatchButton = Button(self.frameBottom, text = "Metric Stopwatch", 
-            font = "Ariel 30", command = self.stopwatch)
+            font = FONT, command = self.stopwatch)
         self.stopwatchButton.grid(row = 0, column = 2)
 
         self.countdownButton = Button(self.frameBottom, text = "Metric Countdown", 
-            font = "Ariel 30", command = self.metricCountDown)
+            font = FONT, command = self.metricCountDown)
         self.countdownButton.grid(row = 1, column = 0)
 
         self.moonButton = Button(self.frameBottom, text = "Moon Phase Calculator MK II", 
-            font = "Ariel 30", command = self.moonCalc)
+            font = FONT, command = self.moonCalc)
         self.moonButton.grid(row = 1, column = 1)
     
         self.eightButton = Button(self.frameBottom, text = "Magic Eight Ball", 
-            font = "Ariel 30", command = self.magicEightBall)
+            font = FONT, command = self.magicEightBall)
         self.eightButton.grid(row = 1, column = 2)
 
         self.craftmineButton = Button(self.frameBottom, text = "Minecraft Calendar Calc", 
-            font = "Ariel 30", command = self.mcCalc)
+            font = FONT, command = self.mcCalc)
         self.craftmineButton.grid(row = 2, column = 0)
 
         self.mayaButton = Button(self.frameBottom, text = "Mayan Long Count Calendar Calc", 
-            font = "Ariel 30", command = self.mayanCalc)
+            font = FONT, command = self.mayanCalc)
         self.mayaButton.grid(row = 2, column = 1)
 
         self.nineButton = Button(self.frameBottom, text = "Maker of Nines", 
-            font = "Ariel 30", command = self.makeNines)
+            font = FONT, command = self.makeNines)
         self.nineButton.grid(row = 2, column = 2)
         
         self.metricToNormButton = Button(self.frameBottom, text = "Metric Date to Normal Date",
-            font = "Ariel 30", command = self.toNormConv)
+            font = FONT, command = self.toNormConv)
         self.metricToNormButton.grid(row = 3, column = 0)
 
     #Quits program.
     def quitButtonAction(self):
         self.window.destroy()
 
-    def metricDateLiveAct(self):            #DONE
+    def metricDateLiveAct(self):            
         metricDateGUILIVE.main()
 
-    def metricCalculator(self):             #DONE
+    def metricCalculator(self):             
         differentMetricDateCalc.main()
 
-    def stopwatch(self):                    #DONE
+    def stopwatch(self):                    
         metricStopwatch.main()
 
-    def metricCountDown(self):              #DONE
+    def metricCountDown(self):              
         metricCountDown.main()
 
-    def moonCalc(self):                     #DONE
+    def moonCalc(self):                     
         newMoonPhaseCalc.main()
 
-    def magicEightBall(self):               #DONE
+    def magicEightBall(self):               
         magicEightBall.main()
 
-    def mcCalc(self):                       #DONE
+    def mcCalc(self):                       
         minecraftCalCalc.main()
 
-    def mayanCalc(self):                    #DONE
+    def mayanCalc(self):                    
         mayanLongCount.main()
 
-    def makeNines(self):                    #DONE
+    def makeNines(self):                    
         nineMaker.main()
 
-    def toNormConv(self):                   #DONE
+    def toNormConv(self):                   
         metricDateToDate.main()
 
 def main():
