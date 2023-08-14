34f337e6addd8db1cc60ca86854c68af07bb3579
KesseNones <jesse.jones@wsu.edu>
2023-08-14 14:52:01 -0700

    Reduced Font Size and Scrubbed Comments of D Pgms

--- dPrograms.py
+++ dPrograms.py
@@ -1,5 +1,5 @@
 #Jesse A. Jones
-#Version: 2023-06-13.16
+#Version: 2023-08-14.91
 
 from tkinter import *
 import dstCalendarCalcGUI
@@ -21,9 +21,11 @@ class D(object):
         self.frameTop = Frame(self.window)
         self.frameTop.pack(side = TOP)
 
+        FONT = "Ariel 20"
+
         #Quit button.
         self.quitButton = Button(self.frameTop, text = "Quit",
-            font = "Ariel 30", command = self.quitButtonAction)
+            font = FONT, command = self.quitButtonAction)
         self.quitButton.pack()
 
         #Holds all other program buttons.
@@ -31,31 +33,31 @@ class D(object):
         self.frameBottom.pack(side = BOTTOM)
 
         self.dstButton = Button(self.frameBottom, text = "DST Calendar Converter", 
-            font = "Ariel 30", command = self.dstCal)
+            font = FONT, command = self.dstCal)
         self.dstButton.grid(row = 0, column = 0)
         
         self.dayButton = Button(self.frameBottom, text = "Day Difference Date Calc", 
-            font = "Ariel 30", command = self.dateDiff)
+            font = FONT, command = self.dateDiff)
         self.dayButton.grid(row = 0, column = 1)
 
         self.dayButton2 = Button(self.frameBottom, text = "DST Rough Nightmare Cycle Calculator", 
-            font = "Ariel 30", command = self.nightCalc)
+            font = FONT, command = self.nightCalc)
         self.dayButton2.grid(row = 0, column = 2)
 
         self.dayButton3 = Button(self.frameBottom, text = "Different Countdown Clock", 
-            font = "Ariel 30", command = self.differentCountdown)
+            font = FONT, command = self.differentCountdown)
         self.dayButton3.grid(row = 1, column = 0)
 
         self.newCountButton = Button(self.frameBottom, text = "Different Countdown Clock MK II", 
-            font = "Ariel 30", command = self.differentCountdownII)
+            font = FONT, command = self.differentCountdownII)
         self.newCountButton.grid(row = 1, column = 1)
 
         self.diffClockButton = Button(self.frameBottom, text = "Different Clock (Not Mine)", 
-            font = "Ariel 30", command = self.diffClk)
+            font = FONT, command = self.diffClk)
         self.diffClockButton.grid(row = 1, column = 2)
 
         self.degreesClockButton = Button(self.frameBottom, text = "360 Degree Clock", 
-            font = "Ariel 30", command = self.degClock)
+            font = FONT, command = self.degClock)
         self.degreesClockButton.grid(row = 2, column = 0)
 
     def quitButtonAction(self):
@@ -64,22 +66,22 @@ class D(object):
     def dstCal(self):                       #DONE (Needs some serious refactoring to look less gross)
         dstCalendarCalcGUI.main()   
 
-    def dateDiff(self):                     #DONE
+    def dateDiff(self):                     
         dateDaysFromOrAgo.main()
 
-    def nightCalc(self):                    #DONE
+    def nightCalc(self):                    
         dstNightmareCycleCalc.main()
     
-    def differentCountdown(self):           #DONE
+    def differentCountdown(self):           
         nonMetricCountDown.main()
 
-    def differentCountdownII(self):         #DONE
+    def differentCountdownII(self):         
         nonMetricCountDown_MKII.main()
 
-    def diffClk(self):                      #DONE
+    def diffClk(self):                      
         differentClock.main()
 
-    def degClock(self):                     #DONE
+    def degClock(self):                     
         degreesClock.main()
 
 def main():
