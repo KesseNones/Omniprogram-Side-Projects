8830d31c2929b92d4532b281d0e4eae0e07c4178
KesseNones <jesse.jones@wsu.edu>
2023-08-14 15:05:24 -0700

    Made Font Smaller for tPrograms.py

--- tPrograms.py
+++ tPrograms.py
@@ -1,5 +1,5 @@
 #Jesse A. Jones
-#Version: 2023-06-13.17
+#Version: 2023-08-14.91
 
 from tkinter import *
 import tempConverterGUI
@@ -19,57 +19,59 @@ class T(object):
         self.frameTop = Frame(self.window)
         self.frameTop.pack(side = TOP)
 
+        FONT = "Ariel 20"
+
         self.quitButton = Button(self.frameTop, text = "Quit",
-            font = "Ariel 30", command = self.quitButtonAction)
+            font = FONT, command = self.quitButtonAction)
         self.quitButton.pack()
 
         self.frameBottom = Frame(self.window)
         self.frameBottom.pack(side = BOTTOM)
 
         self.tempButton = Button(self.frameBottom, text = "Temp Converter", 
-            font = "Ariel 30", command = self.tempConv)
+            font = FONT, command = self.tempConv)
         self.tempButton.grid(row = 0, column = 0)
 
         self.moonButton = Button(self.frameBottom, text = "Time Displayer Ultima", 
-            font = "Ariel 30", command = self.timeUltima)
+            font = FONT, command = self.timeUltima)
         self.moonButton.grid(row = 0, column = 1)
 
         self.TOSButton = Button(self.frameBottom, text = "TOS Stardate", 
-            font = "Ariel 30", command = self.stardateTOS)
+            font = FONT, command = self.stardateTOS)
         self.TOSButton.grid(row = 0, column = 2)
 
         self.TOSButton = Button(self.frameBottom, text = "TOS Stardate Calc", 
-            font = "Ariel 30", command = self.stardateTOSCalc)
+            font = FONT, command = self.stardateTOSCalc)
         self.TOSButton.grid(row = 1, column = 0)
 
         self.trueTNG = Button(self.frameBottom, text = "TNG True Stardate",
-            font = "Ariel 30", command = self.TNGLiveSDate)
+            font = FONT, command = self.TNGLiveSDate)
         self.trueTNG.grid(row = 1, column = 1)
 
         self.trueTNGCalc = Button(self.frameBottom, text = "TNG True Stardate Calculator",
-            font = "Ariel 30", command = self.TNGSDateCalc)
+            font = FONT, command = self.TNGSDateCalc)
         self.trueTNGCalc.grid(row = 1, column = 2)
 
     #Quits program when called.
     def quitButtonAction(self):
         self.window.destroy()
 
-    def tempConv(self):                 #DONE
+    def tempConv(self):                 
         tempConverterGUI.main()
 
-    def timeUltima(self):               #DONE
+    def timeUltima(self):               
         timeDisplayUltima.main()
 
-    def stardateTOS(self):              #DONE
+    def stardateTOS(self):              
         stardateTOS.main()
 
-    def stardateTOSCalc(self):          #DONE
+    def stardateTOSCalc(self):          
         stardateTOSCalc.main()
 
-    def TNGLiveSDate(self):             #DONE
+    def TNGLiveSDate(self):             
         trueStardate.main()
 
-    def TNGSDateCalc(self):             #DONE
+    def TNGSDateCalc(self):             
         trueStardateCalc.main()
 
 def main():
