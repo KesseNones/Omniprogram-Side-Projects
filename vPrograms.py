c1eb843579850131992d1dce82545770035ddfd1
KesseNones <jesse.jones@wsu.edu>
2023-08-14 15:06:58 -0700

    Made Font Smaller for vPrograms.py

--- vPrograms.py
+++ vPrograms.py
@@ -1,5 +1,5 @@
 #Jesse A. Jones
-#Version: 2023-06-13.19
+#Version: 2023-08-14.92
 
 from tkinter import *
 import vikingCalendarCalc
@@ -16,35 +16,37 @@ class V(object):
         self.frameTop = Frame(self.window)
         self.frameTop.pack(side = TOP)
 
+        FONT = "Ariel 20"
+
         self.quitButton = Button(self.frameTop, text = "Quit",
-            font = "Ariel 30", command = self.quitButtonAction)
+            font = FONT, command = self.quitButtonAction)
         self.quitButton.pack()
 
         self.frameBottom = Frame(self.window)
         self.frameBottom.pack(side = BOTTOM)
 
         self.vikingButton = Button(self.frameBottom, text = "Viking Calendar (WIP)", 
-            font = "Ariel 30", command = self.viking)
+            font = FONT, command = self.viking)
         self.vikingButton.grid(row = 0, column = 0)
 
         self.visualButton = Button(self.frameBottom, text = "Visual Clock", 
-            font = "Ariel 30", command = self.visClock)
+            font = FONT, command = self.visClock)
         self.visualButton.grid(row = 0, column = 1)
 
         self.visualCalcButton = Button(self.frameBottom, text = "Visual Clock Calculator", 
-            font = "Ariel 30", command = self.visClockCalc)
+            font = FONT, command = self.visClockCalc)
         self.visualCalcButton.grid(row = 0, column = 2)
 
     def quitButtonAction(self):
         self.window.destroy()
 
-    def viking(self):                           #DONE
+    def viking(self):                           
         vikingCalendarCalc.main()
 
-    def visClock(self):                         #DONE
+    def visClock(self):                         
         visualClock.main()
     
-    def visClockCalc(self):                     #DONE
+    def visClockCalc(self):                     
         visualClockCalc.main()
 
 def main():
