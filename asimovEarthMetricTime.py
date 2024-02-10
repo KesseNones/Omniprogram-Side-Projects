ce8b4ba421b99810f5d2d086f9b8fb210f0d3432
KesseNones <jesse.jones@wsu.edu>
2024-02-09 16:42:43 -0800

    Added asimov metric date calculator program

---
+++ asimovEarthMetricTime.py
@@ -0,0 +1,96 @@
+#Jesse A. Jones
+#Version: 2024-02-10.02
+
+from time import time
+import datetime
+from tkinter import *
+
+#Calculates the current metric date and time based 
+#   on the format used in Isaac Asimov's books.
+class AsimovCalc(object):
+    def __init__(self, window = None):
+        self.window = window
+
+        #Holds quit button.
+        self.frameTop = Frame(self.window)
+        self.frameTop.pack(side = TOP)
+
+        #Quits program if clicked.
+        self.quitButton = Button(self.frameTop, text = "Quit",
+            font = "Ariel 20", command = self.quitButtonAction)
+        self.quitButton.grid(row = 0, column = 0)
+
+        #Holds time and date outputs.
+        self.frameBottom = Frame(self.window)
+        self.frameBottom.pack(side = BOTTOM)
+
+        #Displays time.
+        self.message = Label(self.frameBottom, text = "", font = "Ariel 50", anchor = "w")
+        self.message.grid(row = 0, column = 0)
+        
+        #Displays date
+        self.messageII = Label(self.frameBottom, text = "", font = "Ariel 50", anchor = "w")
+        self.messageII.grid(row = 1, column = 0)
+
+        #Starts the recursive time update loop.
+        self.timeUpdate()
+
+    #Quits program
+    def quitButtonAction(self):
+        self.window.destroy()
+
+    #Finds time and date in Eru'varian calendar and updates stuff as such.
+    def timeUpdate(self):
+        timeArr = self.asimovCalc(self.localUnix(time()))
+        self.message["text"] = timeArr[0]
+        self.messageII["text"] = timeArr[1]
+        self.message.after(1, self.timeUpdate)
+
+    #Takes unix time stamp and alters it based 
+    #   on timezone of user running this application.
+    def localUnix(self, utcUnix):
+        t = utcUnix
+        t = int(t)
+        local = datetime.datetime.now()
+        localHr = local.hour
+        utcHour = ((t % 86400) // 3600)
+        if utcHour < localHr:
+            utcHour += 24
+        timeZoneDiff = abs(utcHour - localHr)
+        t = (t - (3600 * timeZoneDiff))
+        return t
+
+    #Determines the metric date 
+    #   and metric time based on Isaac Asimov's format.
+    def asimovCalc(self, unix):
+        #Number of days from start of CE to start of Unix epoch.
+        baseDays = 719527
+
+        #Finds current metric date.
+        totalDays = baseDays + (unix / 86400)
+        currMetYear = int(totalDays) // 300
+        daysElapsedInMetYear = int(totalDays) % 300
+        currMetMonth = (daysElapsedInMetYear // 30) + 1
+        currMetDay = (daysElapsedInMetYear % 30) + 1
+
+        #Finds current metric time.
+        timeOfDay = totalDays - int(totalDays)
+        currMetHour = int(timeOfDay * 10)
+        currMetMinute = int((timeOfDay * 1000) % 100)
+        currMetSecond = int((timeOfDay * 100000) % 100)
+
+        return [
+            f"{currMetYear}-{str(currMetMonth).zfill(2)}-{str(currMetDay).zfill(2)}",
+            f"{currMetHour}:{str(currMetMinute).zfill(2)}:{str(currMetSecond).zfill(2)}"
+        ]
+
+
+
+def main():
+    root = Tk()
+    root.title("Asimov Metric Time and Calendar")
+    metric = AsimovCalc(root)
+    root.mainloop()
+
+if __name__ == "__main__":
+    main()
